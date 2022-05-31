42f52eff39950e6b1873a49da76f01c3a7d26eea
Dana Zheng <zhengdana1270@gmail.com>
2022-05-30 20:45:02 -0400

    bcrypt and register post method

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
 const express = require('express')
-const res = require('express/lib/response')
 const app = express()
+const bcrypt = require('bcrypt')
+
+const users = []
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))      // allow data in login/register form html tags to be used for req in post methods
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'Dana' })
+    res.render('index.ejs', { name: req.user.name })
 })
 
 app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/login', (req, res) => {
-
-})
-
-app.post('/register', (req, res) => {
-
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
 })
 
 app.listen(3000)
+
+// req.body.____ ==> name='____' (html name tag)
